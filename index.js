2a3535add8ebe0f723991e6d7ecb2ba40b00f82c
DaBusyB <brandiapetsi@gmail.com>
2020-07-06 16:05:37 -0400

    POST and GET working

--- index.js
+++ index.js
@@ -5,44 +5,75 @@ const server = express()
 
 server.use(express.json())
 
-let users = [
-    {
-        id: 1,
-        name: "Jane Doe",
-        bio: "Unknown woman"
-    }
-]
+let users = []
 
 server.get('/api/users', (req, res) => {
-    users ? 
+    users ?
     res.status(200).json(users) :
     res.status(500).json({ errorMessage: "The users information could not be retrieved." })
 })
 
 server.get('/api/users/:id', (req, res) =>{
     const {id} = req.params
-
     const foundUser = users.find(user => user.id)
 
-    foundUser ?
-    res.status(201).json(foundUser) :
-    res.status(404).json({ message: "The user with the specified ID does not exist." })
+    if(foundUser) {
+        res.status(200).json(foundUser)
+    } else if(!foundUser) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else {
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+    }
 })
 
 server.post('/api/users', (req, res) => {
-    userInfo = req.body
+    const userInfo = req.body
 
     userInfo.id = shortid.generate()
 
-    userInfoKeys = Object.keys(userInfo)
+    const userInfoKeys = Object.keys(userInfo)
 
-    if(userInfoKeys.includes(req.body.name && req.body.bio)) {
+    if(!userInfoKeys.includes("name" && "bio")) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else if(userInfoKeys.includes("name" && "bio")) {
         users.push(userInfo)
         res.status(201).json(userInfo)
     } else {
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
     }
-    
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params
+    const changes = req.body
+    const changesKeys = Object.keys(changes)
+
+    const foundUser = users.findIndex(user => user.id)
+
+    if(!changesKeys.includes("name" && "bio")) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else if(foundUser !== -1) {
+        users[foundUser] = changes
+        changes.id = id
+        res.status(200).json(users[foundUser])
+    } else {
+        res.status(500).json({ errorMessage: "The user information could not be modified." })
+    }
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params
+
+    const foundUser = users.find(user => user.id === id)
+
+    if(foundUser) {
+        users = users.filter(user => user.id !== id)
+        res.status(200).json(foundUser)
+    } else if(!foundUser) {
+        res.status(404).json({ errorMessage: "The user information could not be retrieved." })
+    } else {
+        res.status(500).json({ errorMessage: "The user could not be removed" })
+    }
 })
 
 const PORT = 5001
